5cfa8bc47d720c100a4db3dc870e9deec0c00981
Adnan Milisic <adnan@atimsolutions.se>
2021-03-17 19:34:24 +0100

    .

---
+++ ex11/src/index.js
@@ -0,0 +1,34 @@
+
+
+function switchCase(letter){
+ 
+    // Only change code velow this line
+    var animals;
+  switch(letter){
+      case "a":
+          return "antilope";
+          break;
+      case "b":
+          return "bird";
+          break;
+      case "c":
+          return "cat";
+          break;
+      default:
+            return "stuff";
+            break;
+  }
+
+   
+//Only change code above this line
+    return animals;
+}
+
+console.log(switchCase("a"));
+console.log(switchCase("b"));
+console.log(switchCase("c"));
+console.log(switchCase("d"));
+console.log(switchCase("4"));
+
+//Only change code above this line
+module.exports = switchCase;
