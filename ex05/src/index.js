1db943ce33a363073151fc3f78875dd9c97319e3
Adnan Milisic <adnan@atimsolutions.se>
2021-03-17 18:51:09 +0100

    .

---
+++ ex05/src/index.js
@@ -0,0 +1,18 @@
+// Only change code velow this line
+
+function compareStrinValues(m){
+    if(55 !=m){
+      return "Not equal";
+         
+      } 
+      return "Equal";
+      
+  }
+
+console.log(compareStrinValues(55));
+console.log(compareStrinValues("55"));
+console.log(compareStrinValues("21"));
+console.log(compareStrinValues(12));
+console.log(compareStrinValues("Doe"));
+//Only change code above this line
+module.exports = compareStrinValues;
